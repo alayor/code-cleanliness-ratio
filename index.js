de091d547babda25c65f4e9f221ebf1fca469da2
Alonso A. Ortega <alayor3@gmail.com>
2018-11-07 19:10:53 -0700

    Include nested files.

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
 const fs = require('fs')
 
+
+const walkSync = function(dir, filelist) {
+    const files = fs.readdirSync(dir)
+    filelist = filelist || [];
+    files.forEach(function(file) {
+        if (fs.statSync(dir + '/' + file).isDirectory()) {
+            filelist = walkSync(dir + file + '/', filelist);
+        }
+        else {
+            filelist.push(file);
+        }
+    });
+    return filelist;
+};
+
 fs.readFile('domainWords.txt', (err, data) => {
     const domainWords = data.toString().split('\n')
-    fs.readdir('.', (err, files) => {
-        const wordsInDomainWords =
-            files.map(f => domainWords.includes(f) ? 1 : 0)
-                .reduce((acc, curr) => {
-                    return acc + curr
-                })
-        console.log('Your Code Cleanliness Ratio is: %', wordsInDomainWords / files.length / 100)
-    })
+    const files = walkSync('./')
+    const wordsInDomainWords =
+        files.map(f => domainWords.includes(f) ? 1 : 0)
+            .reduce((acc, curr) => {
+                return acc + curr
+            })
+    console.log('Your Code Cleanliness Ratio is: %', wordsInDomainWords / files.length / 100)
 })
 
 
